e92e5eb009385bec2a38aeabb9c691abe335f4bc
An Huynh <An Huynh>
2016-10-14 12:53:34 -0500

    keys, keys, keys

---
+++ keys2.js
@@ -0,0 +1,8 @@
+//console.log('this is loaded');		
+
+module.exports = {
+  consumer_key: 'Ov3Xoim137V8MbVzPnErWQvZK',
+  consumer_secret: 'mNDuHMagW3hr7wzg3RU3i8My8BmfMm5X2EshAlYNDiJas5Xw08',
+  access_token_key: '751687428205719552-4SJeuklQZUInu9rUGOAEm67wjkj9DCi',
+  access_token_secret: 'htjdkR4yDwIW9iOxkWqXPchk7Rj8XDvHLzMsJaxoLxc5H',
+}
